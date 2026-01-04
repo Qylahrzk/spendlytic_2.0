1f9b332b7625325431f7843f08b3db04cd927dd6
Qylahrzk <nuraqilahnur@gmail.com>
2026-01-04 08:48:58 +0800

    important

---
+++ android/app/src/main/kotlin/com/example/spendlytic_v2/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.spendlytic_v2
+
+// ✅ CHANGE 1: Import this specific class
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+// ✅ CHANGE 2: Extend FlutterFragmentActivity instead of FlutterActivity
+class MainActivity: FlutterFragmentActivity() {
+}
